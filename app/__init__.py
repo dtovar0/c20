ace07afa98e114c457fd9f64dd896b2b7c9152f5
Domingo Tovar <dtovar@vivaro.com>
2026-04-17 02:34:11 -0600

    checkpoint: Integrado Flask-Compress para optimización de activos

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,14 @@
 from flask import Flask
+from flask_compress import Compress
 import os
 
 def create_app():
     app = Flask(__name__, 
                 template_folder='../templates', 
                 static_folder='../static')
+
+    # Inicializar Extensiones
+    Compress(app)
 
     # Registro de Blueprints
     from app.modules.core.routes import core_bp
